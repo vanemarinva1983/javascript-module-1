623dcdfbdc7c555f343a4d93b2a01f440dc034c1
vanemarinva1983 <97670848+vanemarinva1983@users.noreply.github.com>
2022-04-18 14:16:15 +0200

    InClass-Homework

--- week-3/Homework/G-array-methods/exercise.js
+++ week-3/Homework/G-array-methods/exercise.js
@@ -3,8 +3,10 @@
   --------------------
 */
 
-var numbers = [3, 2, 1];
-var sortedNumbers; // complete this statement
+const numbers = [3, 2, 1];
+const sortedNumbers = numbers.sort (function (a,b){
+return a - b;
+}); // complete this statement
 
 /* 
   DO NOT EDIT BELOW THIS LINE
---
+++ week-3/InClass/exercise-A.js
@@ -0,0 +1,11 @@
+let array = [ "Javier", "Jorgelina", "Laeken", "Vanessa" ];
+
+console.log(array);
+console.log(array.length);
+
+array.unshift("Yhenifer")
+array.push("Linda")
+
+console.log(array);
+
+
